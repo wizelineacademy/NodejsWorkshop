cdc73f234ededd12292b196c868ef68f23a96255
Alejandro Ramirez <alex.ramirez@wizeline.com>
2017-07-20 00:02:22 -0500

    Add routing

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+// If you don't explicitly name the file you want to
+// require, Node will automatically look for the index.js file
+const routes = require('./routes');
+
+app.use('/', routes);
+
 app.get('/', (req, res) => {
   res.send('Welcome to the kitten API! 😺');
 });
